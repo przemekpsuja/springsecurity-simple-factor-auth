7b63304fdef6807b1e96c57e982ee341e956a888
Przemyslaw Psuja <przemyslaw.psuja@gmail.com>
2020-03-23 19:18:16 +0100

    initial commit

---
+++ src/main/java/com/example/springsecuritysimplefactorauth/SpringsecuritySimpleFactorAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritysimplefactorauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritySimpleFactorAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecuritySimpleFactorAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritysimplefactorauth/SpringsecuritySimpleFactorAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritysimplefactorauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritySimpleFactorAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
